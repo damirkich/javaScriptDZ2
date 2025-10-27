0567e6fc499f5fe2bb167ab4b7c1b0542176be3e
damirkich <damir.chaban.d@gmail.com>
2025-10-27 17:01:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+let celsius = 25;
+let farenheit;
+
+farenheit = celsius * 9/5 + 32;
+
+console.log(`${celsius}C = ${farenheit}F`);
+
+//second task
+
+let daysInMonth = 30;
+
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+
+console.log(`У місяці з ${daysInMonth} днями ${hoursInMonth} годин і ${minutesInMonth} хвилин.`);
+
+//third task
+
+let playerEnergy = 100;
+let playerHealth = 100;
+
+playerEnergy = playerEnergy - 20;
+playerHealth = playerHealth - 40;
+
+console.log(`Енергія гравця впала до ${playerEnergy}, а здоров'я до ${playerHealth}`);
+
+//fourth task
+
+let totalPrice = 8472;
+let discountPercentage = totalPrice * 0.10;
+let discountedPrice = totalPrice - discountPercentage;
+
+console.log(discountedPrice);
+
+//fifth task
+
+const floatNumber = 12.78;
+let roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+// 6th task
+
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+
+// 7th task
+
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+
+// 8th task
+
+let number = 144;
+let sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
+
+// 9th task (не зрозумів як, тому не зрробив)
